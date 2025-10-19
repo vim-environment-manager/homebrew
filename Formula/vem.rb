af99fbffbc4819809e06dae8212c15e1d1e37c9a
Ryo ARIMA <ryo.arima@zohomail.jp>
2025-10-19 21:17:02 +0900

    fix: url

--- Formula/vem.rb
+++ Formula/vem.rb
@@ -4,10 +4,10 @@ class Vem < Formula
   version "0.1.0"
   
   if Hardware::CPU.arm?
-    url "https://vim-environment-manager.github.io/packages/repo/homebrew/archives/vem-0.1.0-202510191002-aarch64.tar.gz"
+    url "https://github.com/ryo-arima/vem/releases/download/v0.1.0-202510191002/vem_Darwin_arm64.tar.gz"
     sha256 "7699f396ea0a5c79ac671c08bf37a3d922ca819facf70e7bbe6cad272bca8a65"
   else
-    url "https://vim-environment-manager.github.io/packages/repo/homebrew/archives/vem-0.1.0-202510191002-x86_64.tar.gz"
+    url "https://github.com/ryo-arima/vem/releases/download/v0.1.0-202510191002/vem_Darwin_x86_64.tar.gz"
     sha256 "607b32d78319db3827d20e9cf2c45b813d48df8593f5f482e69af59b3795f8f8"
   end
 
